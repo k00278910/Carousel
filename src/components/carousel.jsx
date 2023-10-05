33cadbdb4807cac414e5ced7910bdabe1bbb3714
k00278910 <k00278910@student.lit.ie>
2023-10-05 20:44:21 +0100

    take in data prop in App.jsx , map through data items in Carousel.jsx

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,11 +1,17 @@
+/* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import './Carousel.css'
 
-export const Carousel = () => {
+// eslint-disable-next-line react/prop-types
+export const Carousel = ({ data }) => {
+  // eslint-disable-next-line react/prop-types
   return (
     <div>
-      <div>Carousel</div>
+      {data.map((item, idx) => {
+        // eslint-disable-next-line react/jsx-key
+        return <img src={item.src} alt={item.alt} key={idx} />
+      })}
     </div>
   )
 }
