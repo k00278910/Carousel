0f96b3270139eb0d3a5b15c1e2008f72d603d9b2
k00278910 <k00278910@student.lit.ie>
2023-10-05 22:34:23 +0100

    set the variable and state for slides & add conditional classname to only show slide at current index

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,18 +1,29 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, { useState } from 'react'
 import './Carousel.css'
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
 
 // eslint-disable-next-line react/prop-types
 export const Carousel = ({ data }) => {
+  // set variable & state for slide
+  const [slide, setSlide] = useState(0)
+
   // eslint-disable-next-line react/prop-types
   return (
     <div className="carousel">
       <BsArrowLeftCircleFill className="arrow arrow-left" />
       {data.map((item, idx) => {
         // eslint-disable-next-line react/jsx-key
-        return <img src={item.src} alt={item.alt} key={idx} className="slide" />
+        return (
+          <img
+            src={item.src}
+            alt={item.alt}
+            key={idx}
+            // only show slide at current index
+            className={slide === idx ? 'slide' : 'slide slide-hidden'}
+          />
+        )
       })}
       <BsArrowRightCircleFill className="arrow arrow-right" />
       <span className="indicators">
