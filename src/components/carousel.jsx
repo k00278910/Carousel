290f54d8b62340d73d8e48c6ec6e49cc64d8b44b
k00278910 <k00278910@student.lit.ie>
2023-10-05 21:40:20 +0100

    add <BsArrowLeftCircleFill /> & <BsArrowLeftCircleFill /> to Carousel.jsx

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -2,16 +2,19 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
 import './Carousel.css'
+import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs'
 
 // eslint-disable-next-line react/prop-types
 export const Carousel = ({ data }) => {
   // eslint-disable-next-line react/prop-types
   return (
     <div className="carousel">
+      <BsArrowLeftCircleFill />
       {data.map((item, idx) => {
         // eslint-disable-next-line react/jsx-key
         return <img src={item.src} alt={item.alt} key={idx} className="slide" />
       })}
+      <BsArrowRightCircleFill />
     </div>
   )
 }
